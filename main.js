6a442049ffb6fa94a6bca6594d9242d96e551927
Tritolium <paracrafta@gmail.com>
2017-07-05 10:33:43 +0200

    Update main.js

--- main.js
+++ main.js
@@ -21,6 +21,7 @@ module.exports.loop = function () {
     
     
     for(var name in Game.creeps){
+        console.log(name);
         var creep = Game.creeps[name];
         if(creep.memory.role == 'harvester') {
             roleHarvester.run(creep);
